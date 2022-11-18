9f4e1088033ea89dabd3c83b4f081a971849ddc9
Jacky Ngo <jacky.ngo96@gmail.com>
2022-11-18 11:48:28 -0500

    Create Word Search

--- test/test_wordsearch.js
+++ test/test_wordsearch.js
@@ -19,8 +19,23 @@ describe("#wordSearch()", function() {
 
     assert.isFalse(result);
   });
+  it("should return true if the word is present(vertical)", function() {
+    const result = wordSearch([
+      ['F', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['R', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['A', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['N', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['K', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'FRANK')
 
-  it("should return true if the word is present", function() {
+    assert.isTrue(result);
+  });
+
+  it("should return true if the word is present(horizontal)", function() {
     const result = wordSearch([
       ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
       ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
@@ -35,4 +50,6 @@ describe("#wordSearch()", function() {
 
     assert.isTrue(result);
   });
+
 });
+ 
--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,26 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+const transpose = require("/home/labber/lighthouse/async/matrix_transposition.js");
 
-module.exports = wordSearch
+const wordSearch = (letters, word) => {
+  const horizontalJoin = letters.map((h) => h.join(""));
+  const verticalJoin = transpose(letters).map((v) => v.join(""));
+ 
+  for (let horizontal of horizontalJoin) {
+    if (horizontal.includes(word)) {
+      return true;
+    } else {
+      for (let vertical of verticalJoin) {
+        if (vertical.includes(word)) {
+          return true;
+        }
+      }
+    }
+  }
+  return false;
+};
+
+module.exports = wordSearch;
+
+console.log(wordSearch([], 'SEINFELD'))
+
+let hello = []
+console.log(hello.length)
